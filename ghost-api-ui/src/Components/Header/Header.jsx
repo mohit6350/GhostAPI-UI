ab988e8215634682015c58c1ed49c5253b2acdaf
Mohit khare <35894819+mohit6350@users.noreply.github.com>
2024-02-06 21:37:29 +0530

    templates added.

---
+++ ghost-api-ui/src/Components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css';
+
+export default function Header() {
+  return (
+    <div>
+        <div className='header'>
+            <input />
+            <br/>
+            <input />
+        </div>
+    </div>
+  )
+}
